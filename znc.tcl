5e1c558b015405c3a7991f6f67d728df7098764a
ZioNeL <hbo_boy@yahoo.com>
2021-02-10 20:13:32 +0200

    Removal of value $requester from znc:MSG:request

--- znc.tcl
+++ znc.tcl
@@ -1085,7 +1085,7 @@ proc znc:PUB:request {requester host handle chan text} {
 }
 
 proc znc:MSG:request {nick host handle text} {
-        znc:request $nick $host $handle $nick $text $requester
+        znc:request $nick $host $handle $nick $text
 }
 
 ## Confirm Commands
